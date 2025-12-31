6a91feb2ada2a033c177189de69e52529c83ef9d
cyy200422-tech <cyy200422@gmail.com>
2025-12-31 15:44:25 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include "NeuralNetwork.h"
+using namespace std;
+
+
+int main()
+{
+
+	RowVector lay(3, MAT_32SC1);
+	lay.at<int>(0, 0) = 784;
+	lay.at<int>(0, 1) = 30;
+	lay.at<int>(0, 2) = 10;
+
+	NeuralNetwork NN(lay);
+	std::vector<Matrix> training_data, validation_data;
+	NN.loadMnistData(training_data, validation_data);
+	if (!NN.LoadNetworkStructure()) {
+    cout << "无法读取模型文件，检查文件路径" << endl;
+    return -1;
+  }
+	/*NN.SGD(training_data, validation_data, 30, 10, 0.5, 5, true, true, true, true);
+
+	if (NN.SaveNetworkStructure())
+	{
+		cout << "save success!" << endl;
+	}*/
+
+	while (1)
+	{
+		cout << "predict which image?:" << endl;
+		int i = 0;
+		cin >> i;
+		Matrix a = NN.feedforward(validation_data[i]);
+		double max = 0;
+		int max_j = 0;
+		for (int j = 0; j < a.rows; j++)
+		{
+			if (max < a.at<double>(j, 0))
+			{
+				max = a.at<double>(j, 0);
+				max_j = j;
+			}
+		}
+		cout << "predictively:" << max_j << endl;
+		cout << "actually:" << validation_data[i].lables << endl;
+	}
+
+	system("pause");
+	return 0;
+}
